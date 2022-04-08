3bdd2ba77964694da0807bb10ded35069e004713
StuckBoy <zstuck96@gmail.com>
2022-04-08 17:39:36 -0400

    Add New JSON Files for BN6

--- src/main/java/constants/PathConstants.java
+++ src/main/java/constants/PathConstants.java
@@ -5,7 +5,7 @@ public class PathConstants {
     private static final String battleNetworkThreePath = "bn3/BN3 ";
     private static final String battleNetworkSixPath = "bn6/BN6 ";
     private static final String chipLibraryJson = "Chip Library.json";
-    private static final String customizerPartLibraryJson = "Chip Library.json";
+    private static final String customizerPartLibraryJson = "Customizer Part Library.json";
     private static final String programAdvanceJson = "PA Combos.json";
     private static final String exCodeLibraryJson = "Extra Codes.json";
     private static final String errorCodeLibraryJson = "Error Codes.json";
@@ -17,11 +17,12 @@ public class PathConstants {
     public static final String bnThreeCustomizerPartLibrary = bnThreeJsonPath + customizerPartLibraryJson;
     public static final String bnThreeProgramAdvanceLibrary = bnThreeJsonPath + programAdvanceJson;
     public static final String bnThreeExCodes = bnThreeJsonPath + exCodeLibraryJson;
+    public static final String bnThreeCompressionCodeLibrary = bnThreeJsonPath + compressionCodeLibraryJson;
 
     //Battle Network 6 Constants
     public static final String bnSixJsonPath = jsonFilePath + battleNetworkSixPath;
     public static final String bnSixChipLibrary = bnSixJsonPath + chipLibraryJson;
     public static final String bnSixCustomizerPartLibrary = bnSixJsonPath + customizerPartLibraryJson;
     public static final String bnSixProgramAdvanceLibrary = bnSixJsonPath + programAdvanceJson;
-    public static final String bnSixExCodes = bnSixJsonPath + exCodeLibraryJson;
+    public static final String bnSixCompressionCodeLibrary = bnSixJsonPath + compressionCodeLibraryJson;
 }
