cdaa38b72e093bd7aa080f9447fcb555b05794e3
StuckBoy <zstuck96@gmail.com>
2022-05-07 16:22:48 -0400

    Add Helper Method to Simplify Error Messages

--- src/main/java/systems/utility/Helpers.java
+++ src/main/java/systems/utility/Helpers.java
@@ -5,6 +5,20 @@ import pojos.Chip;
 public class Helpers {
 
     /**
+     * Allows for errors to be printed using a single call, while not disrupting
+     * the spacing of messages from the console. I.E. it prints the message,
+     * then a System-dependent line separator.
+     *
+     * @param s The {@link String} we wish to print.
+     * @see #print(String)
+     * @see System#lineSeparator()
+     */
+    public static void errorPrint(String s) {
+        print(s);
+        print(System.lineSeparator());
+    }
+
+    /**
      * Simplifies calls to {@link System} for printing.
      * @param s The complete {@link String} to be printed.
      */
