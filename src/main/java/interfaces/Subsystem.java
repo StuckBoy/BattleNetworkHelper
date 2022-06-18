c5105ced8e40d675049ad8fffd52c794b858d94e
StuckBoy <zstuck96@gmail.com>
2022-06-18 13:54:21 -0400

    Updated Method Signatures Within Subsystem Interface
    Removed redundant portions of method signatures.

--- src/main/java/interfaces/Subsystem.java
+++ src/main/java/interfaces/Subsystem.java
@@ -4,15 +4,14 @@ import java.util.Scanner;
 
 /**
  * Represents a system that performs a wide range of ways to perform a specific
- * task, such as searching for chips or looking up Program Advances. Methods
- * that should appear regardless of the subsystem should be stored here.
+ * task, such as searching for chips or looking up Program Advances.
  */
 public interface Subsystem {
 
     /**
      * Prints the list of available options to the user.
      */
-    public void printOptions();
+    void printOptions();
 
     /**
      * Process the input from the user, and chooses the appropriate search to
@@ -21,5 +20,5 @@ public interface Subsystem {
      * @param input The number inputted by the user.
      * @param keyboard The {@link Scanner} that is reading the user's input.
      */
-    public void processInput(int input, Scanner keyboard);
+    void processInput(int input, Scanner keyboard);
 }
